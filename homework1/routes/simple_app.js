3b63e97c0ac32bb358c7a14efccd793b74f50050
Ashley Deshields <adeshiel@bu.edu>
2018-06-03 15:23:39 -0400

    replaced res.sends with res.json

--- homework1/routes/simple_app.js
+++ homework1/routes/simple_app.js
@@ -6,22 +6,27 @@ router.get('/', function(req, res, next) {
     res.render('index', {title: "Homework 1", yum: "This is the directory for homework 1. Please do /{year} for the JSON file of movies that released that year."})
 })
 
+router.param('string', function (req, res, next, value){
+    req.params.string = value;
+    req.params.len = value.length;
+    res.json(req.params)
+    next()
+})
+
 router.get('/:string', function(req, res, next){
     req.params.len = req.params.string.length;
     console.log(req.params); //gets all params
-    res.send(req.params)
+ //   res.send(req.params)
+    res.json(req.params)
   });
 
-  router.param('string', function (req, res, next, value){
-    req.params.string = value;
-    req.params.len = value.length;
-    next()
-})
+
 
 router.post('/', function (req, res, next) {
     req.body.len = req.body.string.length
     console.log(req.body) //gets string params
-    res.send(req.body)
+    res.json(req.body)
+    
     
 })
 
